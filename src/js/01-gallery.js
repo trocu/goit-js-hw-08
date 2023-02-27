febeda710984a17849bb23aab2a2e1d42849e9ce
Mariusz Trocewicz <throcu@gmail.com>
2023-02-27 02:07:24 +0100

    Add task 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+const onPlay = function (timestamp) {
+  //   console.log(timestamp.seconds);
+  window.localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(timestamp.seconds)
+  );
+};
+
+player.on('timeupdate', onPlay);
+
+// console.log(
+//   JSON.parse(window.localStorage.getItem('videoplayer-current-time'))
+// );
+// console.log(window.localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(window.localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
