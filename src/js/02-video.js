431a66f3b75db5040015891728f9265d594f9d12
Mariusz Trocewicz <throcu@gmail.com>
2023-02-28 01:34:57 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
-const player = new Player('vimeo-player', {
-  id: 19231868,
-  width: 640,
-});
+const player = new Player('vimeo-player', {});
 
-const onPlay = function (timestamp) {
-  //   console.log(timestamp.seconds);
-  window.localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(timestamp.seconds)
-  );
+const TIMESTAMP = 'videoplayer-current-time';
+const onPlay = e => {
+  // console.log(e.seconds);
+  window.localStorage.setItem(TIMESTAMP, e.seconds);
 };
 
-player.on('timeupdate', onPlay);
-
-// console.log(
-//   JSON.parse(window.localStorage.getItem('videoplayer-current-time'))
-// );
-// console.log(window.localStorage.getItem('videoplayer-current-time'));
-player
-  .setCurrentTime(window.localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(window.localStorage.getItem(TIMESTAMP)));
